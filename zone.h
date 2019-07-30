97be20183766ad4d0a417c8d1d690f414b10931e
Augustin Debureaux <adebureaux@gmail.com>
2019-07-30 21:15:03 +0200

    Separate table in zone.c

---
+++ zone.h
@@ -0,0 +1,14 @@
+//
+//  Zones.h
+//  Tarification
+//
+//  Created by Augustin Debureaux on 30/07/2019.
+//  Copyright © 2019 Augustin Debureaux. All rights reserved.
+//
+
+#ifndef zone_h
+#define zone_h
+
+int zone(int, int);
+
+#endif /* Zones_h */
